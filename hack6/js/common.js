a512da7b566ed8f0c3802a0b8ce610c0048f4f77
Momoko002 <83567022+Momoko002@users.noreply.github.com>
2022-03-03 17:13:59 +0900

    Add files via upload

---
+++ hack6/js/common.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.intro',{
+ duration:1600,
+});
+
+ScrollReveal().reveal('.courses',{
+ duration:800,
+});
+
+ScrollReveal().reveal('.images',{
+  duration:800,
+ });
+
+ ScrollReveal().reveal('.access',{
+  duration:800,
+ });
